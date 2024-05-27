cdb57ba70c71fbf0b37ac62477e0e0d45f8700cd
Raafi Hilmi <raafihilmi90@gmail.com>
2024-05-28 05:38:20 +0700

    add img

--- application/views/customer/auth/login_view.php
+++ application/views/customer/auth/login_view.php
@@ -16,7 +16,9 @@
 					<div class="card-body p-0">
 						<!-- Nested Row within Card Body -->
 						<div class="row" style="background-color: #F5F5DC;">
-							<div class="col-lg-5 d-none d-lg-block bg-login-image"></div>
+							<div class="col-lg-5 d-none d-lg-block bg-login-image"
+								style="background-image: url('https://cdn.pixabay.com/photo/2016/05/05/13/10/young-1373879_1280.jpg'); background-size: cover;">
+							</div>
 							<div class="col-lg">
 								<div class="p-5">
 									<div class="text-center">
@@ -25,17 +27,22 @@
 									<?= $this->session->flashdata('message'); ?>
 									<form class="user" action="<?= base_url("login") ?>" method="POST">
 										<div class="form-group">
-											<input type="text" class="form-control form-control-user <?= form_error('email') ? 'is-invalid' : ''; ?>" id="email" name="email" placeholder="Masukkan E-mail..." autofocus>
+											<input type="text"
+												class="form-control form-control-user <?= form_error('email') ? 'is-invalid' : ''; ?>"
+												id="email" name="email" placeholder="Masukkan E-mail..." autofocus>
 											<?= form_error('email', '<div class="invalid-feedback font-weight-bold pl-1">', '</div>') ?>
 										</div>
 										<div class="form-group">
-											<input type="password" class="form-control form-control-user <?= form_error('password') ? 'is-invalid' : ''; ?>" id="password" name="password" placeholder="Password">
+											<input type="password"
+												class="form-control form-control-user <?= form_error('password') ? 'is-invalid' : ''; ?>"
+												id="password" name="password" placeholder="Password">
 											<?= form_error('password', '<div class="invalid-feedback font-weight-bold pl-1">', '</div>') ?>
 										</div>
 										<div class="form-group">
 											<div class="custom-control custom-checkbox small">
 												<input type="checkbox" class="custom-control-input" id="customCheck">
-												<label class="custom-control-label" for="customCheck">Biarkan Masuk</label>
+												<label class="custom-control-label" for="customCheck">Biarkan
+													Masuk</label>
 											</div>
 										</div>
 										<button type="submit" class="btn btn-primary btn-user btn-block">
@@ -44,7 +51,8 @@
 									</form>
 									<hr>
 									<div class="text-center">
-										<a class="small" href="<?= base_url("customer/auth/forgotpassword") ?>">Lupa Password?</a>
+										<a class="small" href="<?= base_url("customer/auth/forgotpassword") ?>">Lupa
+											Password?</a>
 									</div>
 									<div class="text-center">
 										<a class="small" href="<?= base_url("register") ?>">Buat Akun Baru!</a>
